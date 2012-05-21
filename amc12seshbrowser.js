435aa4cb2a1b0a59d6b65a72c85ea676e843f44a
Benjamin Chodoroff <ben@theworkdept.com>
2012-05-21 19:56:46 -0400

    get json. duh.

--- amc12seshbrowser.js
+++ amc12seshbrowser.js
@@ -2,11 +2,11 @@
 
 $(document).ready(function() {
 
-$.load('http://dev.talk070512.phy3.thermitic.net/backbone/rest/views/2012sesh_backbone_user.jsonp?callback=?', function(res) {
+$.getJSON('http://dev.talk070512.phy3.thermitic.net/backbone/rest/views/2012sesh_backbone_user.jsonp?callback=?', function(res) {
   console.log(res);
 });
 
-$.load('http://dev.talk070512.phy3.thermitic.net/amc2012/sessions/taxonomy-js?callback=?', function(res) {
+$.getJSON('http://dev.talk070512.phy3.thermitic.net/amc2012/sessions/taxonomy-js?callback=?', function(res) {
   console.log(res);
 });
 
