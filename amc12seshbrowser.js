ce48e3dec71189e525e89ca0ee3cc5bdc0217569
Benjamin Chodoroff <ben@theworkdept.com>
2012-05-29 18:05:55 -0400

    sorting is better. categories are added.

--- amc12seshbrowser.js
+++ amc12seshbrowser.js
@@ -28,6 +28,7 @@ Seshbro.Models.TaxonomyId = Backbone.Model.extend({});
 Seshbro.Collections.Categories = Backbone.Collection.extend({
   url : "http://talk.alliedmedia.org/amc2012/sessions/taxonomy-js?callback=?",
   comparator : function ( term ) {
+    // we display terms by section, so no need for a sophistasorter
     return term.get("weight");
   }
 });
@@ -45,6 +46,57 @@ Seshbro.Collections.Sessions = Backbone.Collection.extend({
     }
   },
   comparator : function( sesh1, sesh2 ) {
+    if ( sesh1.get('field_2012sched')[0].value > 0 && sesh2.get('field_2012sched')[0].value > 0 ) {
+      var term1 = seshbrodude.categoriesView.collection.where({ tid : sesh1.get('field_2012sched')[0].value })[0];
+      var term2 = seshbrodude.categoriesView.collection.where({ tid : sesh2.get('field_2012sched')[0].value })[0];
+      // ongoing goes to the top
+      if ( 514 == term1.get("tid") == term2.get("tid") ) {
+        return 0;
+      }
+      if ( term1.get("tid") == 514 ) {
+        return -1;
+      }
+      if ( term2.get("tid") == 514 ) {
+        return 1;
+      }
+      // if both terms have parents, then compare the parents first
+      if ( term1.get("parents")[0] > 0 && term2.get("parents")[0] > 0 ) {
+        var term1_p = seshbrodude.categoriesView.collection.where({ tid : term1.get("parents")[0] })[0];
+        var term2_p = seshbrodude.categoriesView.collection.where({ tid : term2.get("parents")[0] })[0];
+        // sesh1 should come after sesh2 if sesh1's parent weighs more
+        if ( term1_p.get("weight") > term2_p.get("weight") ) {
+          return 1;
+        } else if ( term1_p.get("weight") < term2_p.get("weight") ) {
+          return -1;
+        } else if ( term1_p.get("tid") == term2_p.get("tid") ) {
+          // but if they're the same weight (i.e. the same term) then we compare child term weights
+          return term1.get("weight") - term2.get("weight");
+        } else {
+          console.log('broken #1! bad!');
+        }
+      } else if ( term1.get("parents")[0] > 0 && term2.get("parents")[0] == 0 ) {
+        console.log("term1 has parents but term2 doesn't");
+        var term1_p = seshbrodude.categoriesView.collection.where({ tid : term1.get("parents")[0] })[0];
+        return term1_p.get("weight") - term2.get("weight");
+      } else if ( term1.get("parents")[0] == 0 && term2.get("parents")[0] > 0 ) {
+        console.log("term1 doesn't have parents but term2 does");
+        var term2_p = seshbrodude.categoriesView.collection.where({ tid : term2.get("parents")[0] })[0];
+        return term1.get("weight") - term2_p.get("weight");
+      } else if ( term1.get("parents")[0] == 0 && term2.get("parents")[0] == 0 ) {
+        console.log("neither term1 nor term2 have parents. sad.");
+        return term1.get("weight") - term2.get("weight");
+      } else {
+        console.log('broken #2! bad!');
+      }
+    // now let's handle sessions without a schedblock defined
+    } else if ( sesh1.get('field_2012sched')[0].value > 0 && sesh2.get('field_2012sched')[0].value == 0 ) {
+      return -1;
+    } else if ( sesh1.get('field_2012sched')[0].value == 0 && sesh2.get('field_2012sched')[0].value > 0 ) {
+      return 1;
+    } else {
+      return 0;
+    }
+/*
     if ( sesh1.get('field_2012sched')[0].value > 0 && sesh2.get('field_2012sched')[0].value > 0 ) {
       var sesh1_b = seshbrodude.categoriesView.collection.where({ tid : sesh1.get('field_2012sched')[0].value })[0];
       var sesh2_b = seshbrodude.categoriesView.collection.where({ tid : sesh2.get('field_2012sched')[0].value })[0];
@@ -74,6 +126,7 @@ Seshbro.Collections.Sessions = Backbone.Collection.extend({
         }
       }
     }
+*/
   }
 });
 
@@ -88,7 +141,42 @@ Seshbro.Views.Categories = Backbone.View.extend({
   },
   template : doT.template( $( "#seshbro-tpl-categories" ).html() ),
   render : function() {
-    $( "#categories" ).html( this.template( this.collection.toJSON() ) );
+    var allblocks = this.collection.where({ vid : "15" });
+    var blocks = {
+      ongoing : _.filter( allblocks, function( model ) {
+        return model.get("tid") == "514";
+      }),
+      thursday : _.filter( allblocks, function( model ) {
+        return model.get("parents")[0] == 427;
+      }),
+      friday : _.filter( allblocks, function( model ) {
+        return model.get("parents")[0] == 432;
+      }),
+      saturday : _.filter( allblocks, function( model ) {
+        return model.get("parents")[0] == 443;
+      }),
+      sunday : _.filter( allblocks, function( model ) {
+        return model.get("parents")[0] == 453;
+      })
+    };
+    var allt_ps_ng = this.collection.where({ vid : "10" });
+    var t_ps_ng = {
+      t : _.filter( allt_ps_ng, function( model ) {
+        return model.get("parents")[0] == 517;
+      }),
+      ps : _.filter( allt_ps_ng, function ( model ) {
+        return model.get("parents")[0] == 518;
+      }),
+      ng : _.filter( allt_ps_ng, function( model ) {
+        return model.get("tid") == 519;
+      })
+    };
+    var categories = {
+      t_ps_ng : t_ps_ng,
+      blocks : blocks,
+      locations : this.collection.where({ vid : "16" })
+    };
+    $( "#categories" ).html( this.template( categories ) );
     return this;
   }
 });
